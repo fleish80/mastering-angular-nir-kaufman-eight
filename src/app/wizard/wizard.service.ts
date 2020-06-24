f36c4e17b048113bba911884e939a0a724db5aca
DXF0530 <dmitry.fleish@sbdinc.com>
2020-06-24 21:34:30 +0300

    commit-240620202134

--- src/app/wizard/wizard.service.ts
+++ src/app/wizard/wizard.service.ts
@@ -8,11 +8,24 @@ export class WizardService {
 
   constructor(private formBuilder: FormBuilder) {
     this.wizardForm = formBuilder.group({
-      username: [null],
-      email: [null, [Validators.required, WizardValidators.unique(['nir'])]],
+      yearOfBirth: [null],
+      email: [null, [Validators.required]],
       password: [null, [Validators.required]],
       agree: [true]
     });
+    this.registerUserNameUniquenessValidator();
+  }
+
+  registerUserNameUniquenessValidator() {
+    const emailControl = this.getControl('email');
+    this.getControl('yearOfBirth').valueChanges
+      .subscribe((year: number) => {
+        if (year > 1982) {
+          emailControl.setValidators(WizardValidators.unique([year.toString()]));
+        } else {
+          emailControl.clearValidators();
+        }
+      });
   }
 
   getControl(controlName: string): FormControl {
